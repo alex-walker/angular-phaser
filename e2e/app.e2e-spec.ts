0a176ec4356e4bb2e898b456f9d2a39bb639c4bf
Alex Walker <alexwalker@outlook.com>
2017-07-02 16:27:57 +0100

    remove the default html

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angular-phaser App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    expect(page.getParagraphText()).toEqual('Angular Phaser');
   });
 });
