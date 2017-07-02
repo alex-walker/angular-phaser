073197b54b6880aaaa207cf1766a2c443ed041b1
Alex Walker <alexwalker@outlook.com>
2017-07-02 16:43:07 +0100

    display an image using the phaser framework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,9 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
@@ -11,22 +11,22 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
+  it('should create the app', fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'app'`, fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Angular Phaser');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a h1 tag', fakeAsync(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!!');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Phaser');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Game, AUTO } from 'phaser-ce';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Phaser';
+
+  game: Game;
+
+  constructor() {
+    this.game = new Game(800, 600, AUTO, '', { preload: this.preload, create: this.create });
+  }
+
+  preload() {
+    this.game.load.image('logo', 'assets/phaser.png');
+  }
+
+  create() {
+    let logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
+    logo.anchor.setTo(0.5, 0.5);
+  }
+
 }
