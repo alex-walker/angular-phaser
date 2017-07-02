bfb736afa0cd43721b7dafac7411b3e800bc53b2
Angular CLI <angular-cli@angular.io>
2017-07-02 16:09:36 +0100

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AngularPhaserPage } from './app.po';
+
+describe('angular-phaser App', () => {
+  let page: AngularPhaserPage;
+
+  beforeEach(() => {
+    page = new AngularPhaserPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
